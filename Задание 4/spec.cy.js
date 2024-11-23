91676b2600e9238c3ab4e94745d37d2d91fc9a1b
Анна <96340630+Ayn6@users.noreply.github.com>
2024-11-23 09:34:23 +0300

    Add files via upload

--- Задание 4/spec.cy.js
+++ Задание 4/spec.cy.js
@@ -6,25 +6,31 @@ describe("template spec", () => {
     }).as("backtrace");
   });
 
-  it('cy.title() - get the title', () => {
+  it('Проверка заголовка страницы', () => {
 
     cy.title().should('include', 'Школа техник Наруто: стань Хокаге!')
+
   });
 
-  it('cy.url() - get the current URL', () => {
+  it('Проверка url страницы', () => {
 
     cy.url().should('eq', 'https://jut.su/')
+
   })
 
-    it('Поиск аниме по запросу', () => {
+  it('Поиск аниме по запросу', () => {
 
-      cy.get('input[name="ystext"]').first().type('Магическая битва');
-      cy.get('input[value="Найти"]').click();
+    let nameAnime = 'Магическая битва';
 
-      cy.contains('Магическая битва').should('be.visible');
+    cy.get('input[name="ystext"]').first().type(nameAnime);
+    cy.get('input[value="Найти"]').click();
+
+    cy.contains(nameAnime).should('be.visible');
+
   });
 
   it('.as() - alias a DOM element for later use', () => {
+
     cy.visit('https://jut.su/anime/');
 
     cy.get('.all_anime_content')
@@ -37,41 +43,47 @@ describe("template spec", () => {
     cy.get('@firstAnime').click({ multiple: true })
 
     cy.url().should('eq', 'https://jut.su/shingekii-no-kyojin/')
+
   })
 
-  it('.add() - create a custom command', () => {
+  it('Получение всех ссылок на странице', () => {
+
     Cypress.Commands.add('console', {
       prevSubject: true,
     }, (subject, method) => {
-    // the previous subject is automatically received
-    // and the commands arguments are shifted
 
-      // allow us to change the console method used
       method = method || 'log'
-
-      // log the subject to the console
       console[method]('The subject is', subject)
 
-      // whatever we return becomes the new subject
-      // we don't want to change the subject so
-      // we return whatever was passed in
       return subject
     })
 
     cy.get('a').console('info').then(($a) => {
-    // subject is still $button
+
+      $a.each((index, el) => {
+        const $link = Cypress.$(el)
+        if ($link.hasClass('yrw-url')) {
+          console.log(`Ссылка с индексом ${index} имеет класс 'yrw-url'`)
+        } else {
+          console.log(`Ссылка с индексом ${index} не имеет класса 'yrw-url'`)
+        }
+      })
+
     })
+
   })
 
-  it('Страница аниме', () => {
+  it('Проверка открытия стираницы аниме', () => {
 
     cy.get('input[name="ystext"]').first().type('One Piece{enter}');
 
     cy.url().should('include', '/oneepiece/');
     cy.get('h1').should('contain', 'Смотреть Ван Пис все серии');
+
   });
 
   it("Авторизация", () => {
+
     cy.visit("https://jut.su/");
 
     cy.get(".login_btn").click();
@@ -82,9 +94,10 @@ describe("template spec", () => {
     cy.get("#login_submit").click();
 
     cy.get(".user-profile").should("contain", "Профиль пользователя");
+
   });
 
-  it("Регестрация", () => {
+  it("Не валидная регистрация", () => {
     cy.visit("https://jut.su/register.html");
 
     cy.get("#name").type("user");
@@ -99,14 +112,17 @@ describe("template spec", () => {
     cy.contains(
       "Ошибка регистрации"
     ).should("be.visible");
+
   });
 
   it("Воспроизведение видео", () => {
 
-  cy.get('input[name="ystext"]').first().type("Магическая битва {enter}");
-  cy.get(".short-btn").first().click({ force: true });
+    cy.get('input[name="ystext"]').first().type("Магическая битва {enter}");
+    cy.get(".short-btn").first().click({ force: true });
 
-  cy.get(".pm_videojs").click();
-  cy.get("video").should("have.prop", "paused", true);
+    cy.get(".pm_videojs").click();
+    cy.get("video").should("have.prop", "paused", true);
+
   });
+
 });
